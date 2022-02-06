1031a78e726c963c1e86f72635945f7ea49d820c
mizphses <fuminori@mizphses.com>
2022-02-07 01:59:37 +0900

    feat: init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white rounded p-3 w-full mx-8">
+        <p class="text-center text-2xl">PORTAL</p>
+        <p>
+          中央大学コンピュータクラブでは以下のスプレッドシートに掲載している団体にドメインを貸し出しています。
+        </p>
+        <p>
+          ご利用にあたっては審査があります。担当者：尾川（fuminori@mizphses.com）までお問い合わせください。
+        </p>
+        <iframe
+          src="https://docs.google.com/spreadsheets/d/1yHiKXBKv6DAA4ndpefXFgdZJMlRCnu44ilq7DFZXeG0/edit?usp=sharing"
+          width="100%"
+          height="500"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
